d488323f91496628ac54fd6ceabf51ef5c0adbde
ademidov <grjntyjr>
2019-06-22 12:22:21 +0100

    Initial Commit

---
+++ My Locations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  My Locations
+//
+//  Created by demas on 22/06/2019.
+//  Copyright © 2019 demas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
