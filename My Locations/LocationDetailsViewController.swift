888657e8427a1a87299af2df54c56103364de106
ademidov <grjntyjr>
2019-06-28 14:19:56 +0100

    Photo picker

--- My Locations/LocationDetailsViewController.swift
+++ My Locations/LocationDetailsViewController.swift
@@ -28,11 +28,14 @@ class LocationDetailsViewController : UITableViewController, UITextViewDelegate 
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var addPhotoLabel: UILabel!
     
     var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
     var placemark: CLPlacemark?
     var descriptionText = ""
     var categoryName = "No Category"
+    var image: UIImage?
     
     var managedObjectContext: NSManagedObjectContext!
     var date = NSDate()
@@ -56,7 +59,12 @@ class LocationDetailsViewController : UITableViewController, UITextViewDelegate 
         
         if locationToEdit != nil {
             title = "Edit Location"
-        }
+            if locationToEdit?.hasPhoto ?? false {
+                if let image = locationToEdit?.photoImage {
+                    showImage(image: image)
+                }
+            }
+        } 
         
         descriptionTextView.text = ""
         categoryLabel.text = ""
@@ -76,19 +84,22 @@ class LocationDetailsViewController : UITableViewController, UITextViewDelegate 
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
-        if indexPath.section == 0 && indexPath.row == 0 {
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
             return 88
-        }
-        
-        if indexPath.section == 2 && indexPath.row == 2 {
             
+        case (1, _):
+            return imageView.isHidden ? 44 : 280
+            
+        case (2, 2):
             addressLabel.frame.size = CGSize(width: view.bounds.size.width - 115, height: 10000)
             addressLabel.sizeToFit()
             addressLabel.frame.origin.x = view.bounds.size.width - addressLabel.frame.size.width - 15
             return addressLabel.frame.size.height + 20
+            
+        default:
+            return 44
         }
-        
-        return 44
     }
     
     func stringFromPlacemark(placemark: CLPlacemark) -> String {
@@ -116,6 +127,7 @@ class LocationDetailsViewController : UITableViewController, UITextViewDelegate 
         } else {
             hudView.text = "Tagged"
             location = NSEntityDescription.insertNewObject(forEntityName: "Location", into: managedObjectContext) as! Location
+            location.photoID = nil
         }
         
         location.locationDescription = descriptionText
@@ -124,6 +136,20 @@ class LocationDetailsViewController : UITableViewController, UITextViewDelegate 
         location.latitude = coordinate.latitude
         location.date = date
         location.placemark = placemark
+        
+        // Save image
+        if let image = image {
+            if !location.hasPhoto {
+                location.photoID = Location.nextPhotoID() as NSNumber
+            }
+            if let data = image.jpegData(compressionQuality: 0.5) {
+                do {
+                    try data.write(to: location.photoURL, options: .atomic)
+                } catch {
+                    print("Error writing file: \(error)")
+                }
+            }
+        }
         
         do {
             try managedObjectContext.save()
@@ -135,6 +161,7 @@ class LocationDetailsViewController : UITableViewController, UITextViewDelegate 
             // fatalCoreDataError(error)
         }
         
+       
         afterDelay(0.6, run: { self.dismiss(animated: true, completion: nil)})
     }
     
@@ -182,7 +209,82 @@ class LocationDetailsViewController : UITableViewController, UITextViewDelegate 
         
         if indexPath.section == 0 && indexPath.row == 0 {
             descriptionTextView.becomeFirstResponder()
+        } else if indexPath.section == 1 && indexPath.row == 0 {
+            tableView.deselectRow(at: indexPath, animated: true)
+            pickPhoto()
         }
     }
+    
+    func showImage(image: UIImage) {
+        imageView.image = image
+        imageView.isHidden = false
+        imageView.frame = CGRect(x: 10, y: 10, width: 260, height: 260)
+        addPhotoLabel.isHidden = true
+    }
+    
+    
+}
+
+extension LocationDetailsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func takePhotoWithCamera() {
+        
+        let imagePicker = UIImagePickerController();
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func choosePhotoFromLibrary() {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK:- Image Picker Delegates
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        image = info[.editedImage] as? UIImage
+        if let theImage = image {
+            showImage(image: theImage)
+        }
+        tableView.reloadData()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func pickPhoto() {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            showPhotoMenu()
+        } else {
+            choosePhotoFromLibrary()
+        }
+    }
+    
+    func showPhotoMenu() {
+        
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let takePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: {_ in self.takePhotoWithCamera()})
+        alertController.addAction(takePhotoAction)
+        
+        let chooseFromLibraryAction = UIAlertAction(title: "Choose From Library", style: .default, handler: {_ in self.choosePhotoFromLibrary()})
+        alertController.addAction(chooseFromLibraryAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
     
 }
